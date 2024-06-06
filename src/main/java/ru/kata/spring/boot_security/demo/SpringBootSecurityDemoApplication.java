e0f908e618ce23417c853cd8690cdcd3dbc40c33
Pavel Maleev <paul.maleev@gmail.com>
2024-06-06 15:55:01 +0300

    Практическая задача 3.1.3 Java pre-project. Задача 3.1.2. Spring Boot, Security. Выполненно

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,6 +2,8 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import java.awt.*;
 import java.io.IOException;
@@ -11,10 +13,17 @@ import java.net.URISyntaxException;
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+	@EventListener({ApplicationReadyEvent.class})
+	void applicationReadyEvent() {
+		System.out.println("Application started ... launching browser now");
 		System.out.println("http://localhost:8080/");
 		browse("http://localhost:8080/");
+
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 	}
 
 	public static void browse(String url) {
